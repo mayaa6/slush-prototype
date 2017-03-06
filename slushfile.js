3c247bcaf9f3471b7d625a2d0067d556310c6833
Haochen Zhao <haochen@huancheclub.com>
2017-03-06 11:23:08 +0800

    initiate with slush-generator

--- slushfile.js
+++ slushfile.js
@@ -1,8 +1,8 @@
 /*
- * slush-h5bp
- * https://github.com/arvindr21/slush-h5bp
+ * slush-quick-prototype
+ * https://github.com/mayaa6/slush-quick-prototype
  *
- * Copyright (c) 2014, Arvind Ravulavaru
+ * Copyright (c) 2017, mayaa6
  * Licensed under the MIT license.
  */
 
@@ -14,13 +14,70 @@ var gulp = require('gulp'),
     template = require('gulp-template'),
     rename = require('gulp-rename'),
     _ = require('underscore.string'),
-    inquirer = require('inquirer');
+    inquirer = require('inquirer'),
+    path = require('path');
+
+function format(string) {
+    var username = string.toLowerCase();
+    return username.replace(/\s/g, '');
+}
+
+var defaults = (function () {
+    var workingDirName = path.basename(process.cwd()),
+      homeDir, osUserName, configFile, user;
+
+    if (process.platform === 'win32') {
+        homeDir = process.env.USERPROFILE;
+        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+    }
+    else {
+        homeDir = process.env.HOME || process.env.HOMEPATH;
+        osUserName = homeDir && homeDir.split('/').pop() || 'root';
+    }
+
+    configFile = path.join(homeDir, '.gitconfig');
+    user = {};
+
+    if (require('fs').existsSync(configFile)) {
+        user = require('iniparser').parseSync(configFile).user;
+    }
+
+    return {
+        appName: workingDirName,
+        userName: osUserName || format(user.name || ''),
+        authorName: user.name || '',
+        authorEmail: user.email || ''
+    };
+})();
 
 gulp.task('default', function (done) {
     var prompts = [{
+        name: 'appName',
+        message: 'What is the name of your project?',
+        default: defaults.appName
+    }, {
+        name: 'appDescription',
+        message: 'What is the description?'
+    }, {
+        name: 'appVersion',
+        message: 'What is the version of your project?',
+        default: '0.1.0'
+    }, {
+        name: 'authorName',
+        message: 'What is the author name?',
+        default: defaults.authorName
+    }, {
+        name: 'authorEmail',
+        message: 'What is the author email?',
+        default: defaults.authorEmail
+    }, {
+        name: 'userName',
+        message: 'What is the github username?',
+        default: defaults.userName
+    }, {
         type: 'confirm',
         name: 'moveon',
-        message: 'Scaffold a quick web front-end prototype project here?'
+        message: 'Continue?'
     }];
     //Ask
     inquirer.prompt(prompts,
@@ -33,7 +90,7 @@ gulp.task('default', function (done) {
                 .pipe(template(answers))
                 .pipe(rename(function (file) {
                     if (file.basename[0] === '_') {
-                        file.basename = '' + file.basename.slice(1);
+                        file.basename = '.' + file.basename.slice(1);
                     }
                 }))
                 .pipe(conflict('./'))
